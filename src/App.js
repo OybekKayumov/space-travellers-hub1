a1dbdc32f4a7cc24b9c713155bfadc9d918c6c01
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-05-10 05:02:19 -0500

    Add Router Routes Route to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Missions from './pages/Missions';
+import Rockets from './pages/Rockets';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello</h2>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+
+        <Route path="rockets" element={<Rockets />} />
+        <Route path="missions" element={<Missions />} />
+        <Route path="profile" element={<Profile />} />
+
+      </Routes>
+    </Router>
   );
 }
 
