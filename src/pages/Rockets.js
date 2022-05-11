a2fe281c0fb29384d89e1d3e501b3311003191f5
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-11 13:28:36 +0500

    add display rockets

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import RocketItem from '../components/rockets/elementRockets';
+import styles from '../components/rockets/Rockets.module.css';
+
+const rocketList = useSelector((state) => state.rocketReducer);
 
 function Rockets() {
   return (
-    <div>
-      Rockets
-    </div>
+    <ul className={styles.flex}>
+      { rocketList.map((item) => (
+        <RocketItem
+          desc={item.desc}
+          key={item.id}
+          id={item.id}
+          name={item.name}
+          image={item.flickr_images}
+          reserved={item.reservation}
+        />
+      ))}
+    </ul>
   );
 }
 
