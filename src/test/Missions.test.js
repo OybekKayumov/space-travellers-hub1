df8125e4ea0a0c4109c80621e3e003d8a5e54ba4
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-05-12 05:31:56 -0500

    Add snapShot test to Missions.test.js file

--- src/test/Missions.test.js
+++ src/test/Missions.test.js
@@ -1,8 +1,35 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-redux';
+import elementStore from '../redux/configureStore';
+import Missions from '../pages/Missions';
 
 const MissionsList = () => <>MissionsList</>;
 
 test('should display the nav text Description', () => {
   render(<MissionsList />);
   screen.debug();
+});
+
+const missions = [
+  {
+    desc: 'The SpaceX Missions are good plan for system',
+    key: 'cheers',
+    id: 'csgo',
+    type: 'hard',
+    name: 'care',
+    image: 'url',
+    reservation: false,
+  },
+];
+
+describe('Test renders the Missions component', () => {
+  it('missions section renders a hardcoded object, passed as a prop', () => {
+    const { container } = render(
+      <Provider store={elementStore}>
+        <Missions key={missions[0]} mission={missions[0]} />
+      </Provider>,
+    );
+    expect(container).toMatchSnapshot();
+  });
 });
